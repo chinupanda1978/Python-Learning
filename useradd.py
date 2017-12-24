9b86d2ca13b97b728217ccafa2cb87e2bedbedb2
chinupanda1978 <chinupanda1978@gmail.com>
2017-12-24 17:12:58 +0530

    multiping

--- useradd.py
+++ useradd.py
@@ -1,10 +1,11 @@
 #!/bin/python
 import os
-for i in ["sashi", "chinu", "ajaya", "ganesh", "dipti"]:
-	V_Ret=os.system("id "+i+" 1> /dev/null 2> /dev/null")
+for i in ["sashi","chinu","dipti"]:
+	V_Ret=os.system("id "+i+" 1>/dev/null 2>/dev/null")
 	if V_Ret==0:
 		print(i, "User is already exist...")
 	else:
 		os.system("useradd "+i)
 
 		print(i, "User created successfully...")
+exit()
